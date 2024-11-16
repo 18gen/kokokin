ee32f80af16fd7e3cf7c76482df7bcbcfceba583
18gen <ichihashigen@gmail.com>
2024-11-16 04:11:16 -0500

    Initial Commit

---
+++ kokokin/kokokinApp.swift
@@ -0,0 +1,17 @@
+//
+//  kokokinApp.swift
+//  kokokin
+//
+//  Created by Gen Ichihashi on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct kokokinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kokokinTests/kokokinTests.swift
@@ -0,0 +1,17 @@
+//
+//  kokokinTests.swift
+//  kokokinTests
+//
+//  Created by Gen Ichihashi on 2024-11-16.
+//
+
+import Testing
+@testable import kokokin
+
+struct kokokinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
